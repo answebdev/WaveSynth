fca89048cd3934cf46210454e0ad03736b82cbca
Adolf Schmuck <britvaica@netscape.net>
2025-01-11 12:38:22 -0800

    clear buffer

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -91,10 +91,11 @@ void WaveSynthAudioProcessor::changeProgramName (int index, const juce::String& 
 }
 
 //==============================================================================
-void WaveSynthAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void WaveSynthAudioProcessor::prepareToPlay (double sampleRate)
 {
     // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    // initialisation that you need.
+    synth.prepareToPlay(sampleRate);
 }
 
 void WaveSynthAudioProcessor::releaseResources()
@@ -132,30 +133,11 @@ bool WaveSynthAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts
 void WaveSynthAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+    // Clear buffer of any samples we already have
+    buffer.clear();
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    synth.processBlock(buffer, midiMessages)
 }
 
 //==============================================================================
