bb5e78bd1aedab7d2d592a995266bb054356a705
Adolf Schmuck <britvaica@netscape.net>
2025-01-11 19:28:18 -0800

    update code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void WaveSynthAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("WaveSynth", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void WaveSynthAudioProcessorEditor::resized()
