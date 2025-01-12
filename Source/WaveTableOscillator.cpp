151059cbbb3da112aae26aed078dae9855040bb4
Adolf Schmuck <britvaica@netscape.net>
2025-01-11 17:04:55 -0800

    add linear interpolation code

--- Source/WaveTableOscillator.cpp
+++ Source/WaveTableOscillator.cpp
@@ -17,6 +17,17 @@ void WaveTableOscillator::setFrequency(float fequency) {
 float WaveTableOscillator::getSample() {
 	const auto sample = interpolateLinearly();
 	index += indexIncrement;
-	index = std::fmod(index, waveTable.size());
+	index = std::fmod(index, static_cast<float>(waveTable.size()));
 	return sample;
+}
+
+float WaveTableOscillator::interpolateLinearly()
+{
+	const auto truncatedIndex = static_cast<int>(index);
+	const auto nextIndex = (truncatedIndex + 1) % static_cast<int>(waveTable.size());
+	const auto nextIndexWeight = index - static_cast<float>(truncatedIndex);
+	const auto trucatedIndexWeight = 1.f - nextIndexWeight;
+
+	return trucatedIndexWeight * waveTable[truncatedIndex] + nextIndexWeight * waveTable[nextIndex];
+
 }
--- Source/WaveTableOscillator.h
+++ Source/WaveTableOscillator.h
@@ -8,6 +8,7 @@ public:
 	void setFrequency(float frequency);
 	float getSample();
 private:
+	float interpolateLinearly();
 	std::vector<float> waveTable;
 	double sampleRate;
 	float index = 0.f;
